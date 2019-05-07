3b398864e854c3370dcddfd88980f8fd470b645a
Mystified131 <mystifiedthomas@gmail.com>
2019-05-07 13:12:36 -0500

    Initial commit.

---
+++ PainterGraph/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.Window
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(186, 117)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.Window
+        Me.Button2.Location = New System.Drawing.Point(192, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(186, 117)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.Window
+        Me.Button3.Location = New System.Drawing.Point(384, 0)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(186, 117)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.Window
+        Me.Button4.Location = New System.Drawing.Point(576, 0)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(186, 117)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.SystemColors.Window
+        Me.Button5.Location = New System.Drawing.Point(0, 123)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(186, 117)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.SystemColors.Window
+        Me.Button6.Location = New System.Drawing.Point(192, 123)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(186, 117)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.SystemColors.Window
+        Me.Button7.Location = New System.Drawing.Point(384, 123)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(186, 117)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.SystemColors.Window
+        Me.Button8.Location = New System.Drawing.Point(576, 123)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(186, 117)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.SystemColors.Window
+        Me.Button9.Location = New System.Drawing.Point(0, 246)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(186, 117)
+        Me.Button9.TabIndex = 8
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.SystemColors.Window
+        Me.Button10.Location = New System.Drawing.Point(192, 246)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(186, 117)
+        Me.Button10.TabIndex = 9
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.SystemColors.Window
+        Me.Button11.Location = New System.Drawing.Point(384, 246)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(186, 117)
+        Me.Button11.TabIndex = 10
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.SystemColors.Window
+        Me.Button12.Location = New System.Drawing.Point(576, 246)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(186, 117)
+        Me.Button12.TabIndex = 11
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.BackColor = System.Drawing.SystemColors.Window
+        Me.Button13.Location = New System.Drawing.Point(0, 369)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(186, 117)
+        Me.Button13.TabIndex = 12
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.SystemColors.Window
+        Me.Button14.Location = New System.Drawing.Point(192, 369)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(186, 117)
+        Me.Button14.TabIndex = 13
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.BackColor = System.Drawing.SystemColors.Window
+        Me.Button15.Location = New System.Drawing.Point(384, 369)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(186, 117)
+        Me.Button15.TabIndex = 14
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.SystemColors.Window
+        Me.Button16.Location = New System.Drawing.Point(576, 369)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(186, 117)
+        Me.Button16.TabIndex = 15
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(766, 494)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "PainterGraph"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+End Class
---
+++ PainterGraph/Form1.vb
@@ -0,0 +1,131 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button1_MouseHover(sender As Object, e As EventArgs) Handles Button1.MouseHover
+        Button1.BackColor = Color.White
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button2.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button2_MouseHover(sender As Object, e As EventArgs) Handles Button2.MouseHover
+        Button2.BackColor = Color.White
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button3.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button3_MouseHover(sender As Object, e As EventArgs) Handles Button3.MouseHover
+        Button3.BackColor = Color.White
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Button4.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button4_MouseHover(sender As Object, e As EventArgs) Handles Button4.MouseHover
+        Button4.BackColor = Color.White
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Button5.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button5_MouseHover(sender As Object, e As EventArgs) Handles Button5.MouseHover
+        Button5.BackColor = Color.White
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Button6.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button6_MouseHover(sender As Object, e As EventArgs) Handles Button6.MouseHover
+        Button6.BackColor = Color.White
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Button7.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button7_MouseHover(sender As Object, e As EventArgs) Handles Button7.MouseHover
+        Button7.BackColor = Color.White
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Button8.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button8_MouseHover(sender As Object, e As EventArgs) Handles Button8.MouseHover
+        Button8.BackColor = Color.White
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Button9.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button9_MouseHover(sender As Object, e As EventArgs) Handles Button9.MouseHover
+        Button9.BackColor = Color.White
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Button10.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button10_MouseHover(sender As Object, e As EventArgs) Handles Button10.MouseHover
+        Button10.BackColor = Color.White
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Button11.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button11_MouseHover(sender As Object, e As EventArgs) Handles Button11.MouseHover
+        Button11.BackColor = Color.White
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Button12.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button12_MouseHover(sender As Object, e As EventArgs) Handles Button12.MouseHover
+        Button12.BackColor = Color.White
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Button13.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button13_MouseHover(sender As Object, e As EventArgs) Handles Button13.MouseHover
+        Button13.BackColor = Color.White
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Button14.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button14_MouseHover(sender As Object, e As EventArgs) Handles Button14.MouseHover
+        Button14.BackColor = Color.White
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Button15.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button15_MouseHover(sender As Object, e As EventArgs) Handles Button15.MouseHover
+        Button15.BackColor = Color.White
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Button16.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button16_MouseHover(sender As Object, e As EventArgs) Handles Button16.MouseHover
+        Button16.BackColor = Color.White
+    End Sub
+
+
+End Class
